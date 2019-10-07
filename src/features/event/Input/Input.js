60b9a02d26e2551ea81a2df306c75ec56e8d253d
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2019-10-07 20:59:38 +0200

    Styles have been improved

--- src/features/event/Input/Input.js
+++ src/features/event/Input/Input.js
@@ -1,9 +1,10 @@
 import React from "react";
 import InputToGallery from "./InputToGallery";
-
+import { Divider } from 'semantic-ui-react';
 
 const Input = () => (
-    <>    
+    <>   
+        <Divider style={{ marginLeft: '40px' }}/> 
         <InputToGallery/>
     </>
 );
